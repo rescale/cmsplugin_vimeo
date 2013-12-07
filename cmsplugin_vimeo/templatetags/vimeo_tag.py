2bbc87313992242cdc8a934be1c0767b8ee608a5
John Park <john@rescale.com>
2013-12-06 18:34:56 -0800

    changed templatetag file name

---
+++ cmsplugin_vimeo/templatetags/vimeo_tag.py
@@ -0,0 +1,21 @@
+from django import template
+from cmsplugin_vimeo.models import Vimeo
+
+
+register = template.Library()
+
+
+class LatestVideosNode(template.Node):
+    def __init__(self, count):
+        self.count = count
+
+    def render(self, context):
+        videos = Vimeo.objects.order_by('-id')[:self.count]
+        context['videos'] = videos
+        return ''
+
+
+@register.tag
+def get_latest_videos(parser, token):
+    tag_name, count = token.split_contents()
+    return LatestVideosNode(int(count))
