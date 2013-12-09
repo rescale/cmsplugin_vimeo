697dfa8347778f9ae3717f0045ec468e201c528b
John Park <john@rescale.com>
2013-12-09 13:37:04 -0800

    modified templatetags to return associated page info

--- cmsplugin_vimeo/templatetags/vimeo_tags.py
+++ cmsplugin_vimeo/templatetags/vimeo_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from cmsplugin_vimeo.models import Vimeo
+import requests
 
 
 register = template.Library()
@@ -10,7 +11,15 @@ class LatestVideosNode(template.Node):
         self.count = count
 
     def render(self, context):
-        videos = Vimeo.objects.order_by('-id')[:self.count]
+        videos = list(Vimeo.objects.order_by('-id')[:self.count])
+
+        for v in videos:
+            r = requests.get('https://vimeo.com/api/v2/video/%s.json'
+                             % v.video_id)
+            v.page_title = v.page.get_menu_title()
+            v.page_link = v.page.get_absolute_url()
+            v.thumbnail = r.json()[0]['thumbnail_large']
+
         context['videos'] = videos
         return ''
 
